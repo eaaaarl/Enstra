37c4d31a57d2b1bb1542a8918585d256727f5b2d
eaaaarl <dev.earl00@gmail.com>
2025-05-02 19:30:02 +0800

    chore: Update server port, and apply functional changes

--- client/src/app/Providers.tsx
+++ client/src/app/Providers.tsx
@@ -4,6 +4,7 @@ import { Provider as ReduxProvider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import store from "@/lib/redux/store";
+import { Toaster } from "@/components/ui/sonner";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -16,6 +17,7 @@ function Providers({ children }: ProvidersProps) {
     <BrowserRouter>
       <ReduxProvider store={store}>
         <PersistGate persistor={persistor} loading={null}>
+          <Toaster />
           {children}
         </PersistGate>
       </ReduxProvider>
--- client/src/features/auth/hooks/useSignIn.ts
+++ client/src/features/auth/hooks/useSignIn.ts
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useForm } from "react-hook-form";
 import { signInSchema, signInValues } from "../schema/signin.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSignInMutation } from "../api/authApi";
+import { useAppDispatch } from "@/lib/redux/hooks";
+import { toast } from "sonner";
+import { setUser } from "@/lib/redux/states/authSlice";
+import { useNavigate } from "react-router-dom";
 
 export const useSignIn = () => {
   const form = useForm<signInValues>({
@@ -10,9 +16,21 @@ export const useSignIn = () => {
       password: "",
     },
   });
-
-  const callSignIn = (payload: signInValues) => {
-    console.log(payload);
+  const navigate = useNavigate();
+  const [signIn, { isLoading }] = useSignInMutation();
+  const dispatch = useAppDispatch();
+  const callSignIn = async (payload: signInValues) => {
+    try {
+      const res = await signIn(payload).unwrap();
+      const authuser = res.data;
+      dispatch(setUser(authuser));
+      toast.success(res.message);
+      navigate("/home");
+    } catch (error: any) {
+      toast.error(
+        error?.data?.message || "Failedt to sign in, Please try again"
+      );
+    }
   };
 
   const handleSubmit = (payload: signInValues) => {
@@ -22,5 +40,6 @@ export const useSignIn = () => {
   return {
     form,
     handleSubmit,
+    isLoading,
   };
 };
