29e4c1107c585258bcabbdde708c61fed8967ac8
eaaaarl <dev.earl00@gmail.com>
2025-05-06 22:30:43 +0800

    📦 feat: create student data table

--- client/src/components/Pages/Admin/StudentPage.tsx
+++ client/src/components/Pages/Admin/StudentPage.tsx
@@ -1,10 +1,21 @@
 import { Header } from "@/components/Navigation/Admin";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import StudentTable from "@/features/admin/components/StudentTable";
 
 function StudentPage() {
   return (
     <>
       <Header content="Students Management" title="Student" />
-      StudentPage
+      <div className="mx-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Student List</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <StudentTable />
+          </CardContent>
+        </Card>
+      </div>
     </>
   );
 }
